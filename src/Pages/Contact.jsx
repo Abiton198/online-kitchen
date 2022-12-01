2f7f403994bd1c40bad815e150d217534bcd86ef
abiton198 <abitonp@gmail.com>
2022-12-01 12:46:53 +0200

    .

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,31 +1,46 @@
 import React from 'react'
+import foodmy from '../img/foodmy.png'
 import foodmyway from '../img/foodmyway.jpg'
 
 export default function Contact() {
   return (
     <div className='contacts'>
+            <div className='heading-contact'>
+                <h1>Find Us</h1>
+                    </div>
 
-            <h1>Find Us</h1>
-            <img src={foodmyway} alt='Food My Way' width='200px' className='contact-image'/>
-        <ul>
-            <li className='social_item'>
-                <a className='whatsapp' href="http://wa.me/0027761272025"> 
-                    <i className="fab fa-whatsapp"></i>
-                        </a> Let's talk now!
-                            </li>
+        <div className='img-icons'>
 
-            <li className="social_item">
-                <a className="twitter" href="https://web.facebook.com/zanele.saul.5">
-                    <i className="fab fa-facebook"></i>
-                        </a> Find us on Facebook!
-                            </li>
+            <div className='contact-img'>
+                <img src={foodmyway} alt='Food My Way' width='200px' className='contact-image'/>
+                    </div>
+        
+            <div className='contact-icons'>
+                    <ul>
+                        <li className='social_item'>
+                            <a className='whatsapp' href="http://wa.me/0027761272025"> 
+                                <i className="fab fa-whatsapp"></i>
+                                    </a> Let's talk now!
+                                        </li>
 
-            <li className="social_item">
-                <a className="instagram" href='https://www.instagram.com/i_am_zazat/'>
-                    <i className="fab fa-instagram"></i>
-                        </a> Here we are on Instagram!
-                            </li>         
-        </ul>
+                        <li className="social_item">
+                            <a className="twitter" href="https://web.facebook.com/zanele.saul.5">
+                                <i className="fab fa-facebook"></i>
+                                    </a> Find us on Facebook!
+                                        </li>
+
+                        <li className="social_item">
+                            <a className="instagram" href='https://www.instagram.com/i_am_zazat/'>
+                                <i className="fab fa-instagram"></i>
+                                    </a> View us on Instagram!
+                                        </li>         
+                    </ul>
+            </div> 
+
+             <div className='contact-img'>
+                <img src={foodmy} alt='Food My Way' width='240px' className='contact-image'/>
+                    </div>           
+        </div>
 
 
     </div>
