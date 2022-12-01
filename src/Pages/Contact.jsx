697bd30d17efa63b44d54e05b71644c15beb7080
abiton198 <abitonp@gmail.com>
2022-12-01 07:29:46 +0200

    .

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -9,20 +9,22 @@ export default function Contact() {
             <img src={foodmyway} alt='Food My Way' width='200px' className='contact-image'/>
         <ul>
             <li className='social_item'>
-                <a className='whatsapp' href="http://wa.me/0027761272025">
+                <a className='whatsapp' href="http://wa.me/0027761272025"> 
                     <i className="fab fa-whatsapp"></i>
-                        </a>
+                        </a> Let's talk now!
                             </li>
 
             <li className="social_item">
                 <a className="twitter" href="https://web.facebook.com/zanele.saul.5">
                     <i className="fab fa-facebook"></i>
-                        </a></li>
+                        </a> Find us on Facebook!
+                            </li>
 
             <li className="social_item">
                 <a className="instagram" href='https://www.instagram.com/i_am_zazat/'>
                     <i className="fab fa-instagram"></i>
-                        </a></li>         
+                        </a> Here we are on Instagram!
+                            </li>         
         </ul>
 
 
